d61e4356552347cf2b0add545bcaaf6b556615af
avivharuzi <avivharuzi@gmail.com>
2019-03-21 11:48:55 +0200

    Add .env.example and update indention to 2 spaces

--- src/server.js
+++ src/server.js
@@ -1,17 +1,11 @@
 import { config } from 'dotenv';
-
 config();
 
 import app from './app';
-import http from 'http';
 
 const hostname = process.env.HOSTNAME || '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-app.set('port', port);
-
-const server = http.createServer(app);
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
 });
